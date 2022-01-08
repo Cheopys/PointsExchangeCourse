41f5c9d0be6f710ba44ecd8e321236e07785baae
Chris Fox <cheopys@gmail.com>
2022-01-08 20:12:39 +0700

    move out of client folder

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,3 +1,6 @@
+var Escrow = artifacts.require("./Escrow.sol");
+var EscrowExchange = artifacts.require("./EscrowExchange.sol");
+
 var SimpleStorage = artifacts.require("./SimpleStorage.sol");
 
 module.exports = function(deployer) {
